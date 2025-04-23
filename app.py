98b38f58af434f9f63b41009e92d14c587b885a0
soumyaafoussi0224 <afoussisoumya0224@gmail.com>
2025-04-24 01:06:51 +0200

    modification

--- app.py
+++ app.py
@@ -371,4 +371,5 @@ def page_not_found(e):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
